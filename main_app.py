99b7430298448b535523356213e009c7171a6c55
bosssizu <bsibaja10@gmail.com>
2025-08-27 14:22:57 -0600

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.middleware.cors import CORSMiddleware
 
-APP_VERSION = "v40q-ui-clean"
+APP_VERSION = "v40r-dom-inject"
 
 app = FastAPI(title="GanadoBravo API", version=APP_VERSION)
 
